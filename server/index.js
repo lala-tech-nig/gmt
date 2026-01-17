79876374fa1f785e4ae4456bd4c7f3c22b9a4c6f
LALA TECH <olaniyanshamsudeenyinka001@gmail.com>
2026-01-17 21:21:15 +0100

    cors

--- server/index.js
+++ server/index.js
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: ['https://gmt-two-roan.vercel.app', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(helmet());
 app.use(morgan('dev'));
 
